c1453d67f4e57479d351c4a21d34849b9cc47bdb
edik625 <ednari.turmanidze@gmail.com>
2024-06-07 22:34:42 +0400

    create new models 'climat'

--- country/models.py
+++ country/models.py
@@ -5,5 +5,13 @@ from django.db import models
 class Continent(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__ (self):
+        return self.name
+
 
     
+class Climat (models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__ (self):
+        return self.name
