0f874604e3744ae5ed09874322c9388e8973d1bb
edik625 <ednari.turmanidze@gmail.com>
2024-06-08 15:27:31 +0400

    projects add new fails views.py

--- country/views.py
+++ country/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Climat, Continent, Country
+from .serializers import ClimatSerializer, ContinetSerializer, CountrySerializer
+from rest_framework import viewsets
 # Create your views here.
+
+
+class ClimatViewSet(viewsets.ModelViewSet):
+    queryset = Climat.objects.all()
+    serializer_class = ClimatSerializer
+
+
+class ContinentViewSet(viewsets.ModelViewSet):
+    queryset = Continent.objects.all()
+    serializer_class = ContinetSerializer
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Continent.objects.all()
+    serializer_class = ContinetSerializer
